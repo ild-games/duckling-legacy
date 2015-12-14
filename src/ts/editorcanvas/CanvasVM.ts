22bc3b11124ec7d0584d3b9f69b99d959159c149
Jeff <jeffswenson@betterthannull.com>
2015-12-14 15:44:09 -0600

    Improve error handling in promises.

--- src/ts/editorcanvas/CanvasVM.ts
+++ src/ts/editorcanvas/CanvasVM.ts
@@ -72,7 +72,7 @@ module editorcanvas {
             this._systemLoader.loadMap(this._project.projectName, this.data.getEmptyClone())
                 .then((entitySystem : entityframework.EntitySystem) => {
                     this.changeData(entitySystem);
-                });
+                }, framework.error.onPromiseError(this._context));
         }
 
         private changeData(newData : entityframework.EntitySystem) {
--- src/ts/framework/error/ErrorHandler.ts
+++ src/ts/framework/error/ErrorHandler.ts
@@ -7,8 +7,20 @@ module framework.error {
         window.addEventListener("error", onUncaughtException(window));
     }
 
+    /*
+     * Create an error handler for a promise.
+     * @param context Context that the error is occuring in.
+     */
+    export function onPromiseError(context : Context) {
+        return function(error) {
+            window.console.log(error);
+            alert(error.stack);
+        };
+    }
+
     function onUncaughtException(window) {
         return function (e : ErrorEvent) {
+            window.console.log(e);
             window.alert(e.error.stack);
         }
     }
