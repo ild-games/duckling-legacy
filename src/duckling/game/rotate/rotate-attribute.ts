ee336e8eb953d385dbc951cd254d674257fc17af
Tucker <tlein@radford.edu>
2016-08-30 11:17:42 -0500

    add rotate attribute

---
+++ src/duckling/game/rotate/rotate-attribute.ts
@@ -0,0 +1,15 @@
+import {Entity, Attribute} from '../../entitysystem/entity';
+
+export const ROTATE_KEY = "rotate";
+
+export interface RotateAttribute extends Attribute {
+    speed: number;
+}
+
+export var defaultRotate : RotateAttribute = {
+    speed: 0
+}
+
+export function getRotate(entity : Entity) : RotateAttribute {
+    return <RotateAttribute>entity[ROTATE_KEY];
+}
---
+++ src/duckling/game/rotate/rotate.component.ts
@@ -0,0 +1,31 @@
+import {
+    Component,
+    Input,
+    Output,
+    EventEmitter
+} from '@angular/core';
+
+import {NumberInput} from '../../controls';
+import {immutableAssign} from '../../util';
+
+import {RotateAttribute} from './rotate-attribute';
+
+@Component({
+    selector: "dk-rotate-component",
+    directives: [NumberInput],
+    template: `
+        <dk-number-input
+            label="Speed"
+            [value]="attribute.speed"
+            (validInput)="onSpeedInput($event)">
+        </dk-number-input>
+    `
+})
+export class RotateComponent {
+    @Input() attribute : RotateAttribute;
+    @Output() attributeChanged = new EventEmitter<RotateAttribute>();
+
+    onSpeedInput(newSpeed : number) {
+        this.attributeChanged.emit(immutableAssign(this.attribute, {speed: newSpeed}))
+    }
+}
