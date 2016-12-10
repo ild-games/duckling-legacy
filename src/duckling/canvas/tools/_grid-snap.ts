cc4508a01dd4c4e3d8022d70adae27c8581b5cdc
Tucker Lein <self@tuckerle.in>
2016-12-10 16:24:47 -0600

    Snap to grid (#200)
    * implement snap to grid
    
    * rebase to get new method of saving map's grid size
    
    * Rewrite snap to grid math.
    
    * fix code review issues

---
+++ src/duckling/canvas/tools/_grid-snap.ts
@@ -0,0 +1,27 @@
+import {roundToMultiple} from '../../math/number-utils';
+import {Vector, vectorModulus, vectorAdd, vectorMultiply} from '../../math/vector';
+import {Box2} from '../../math/box2';
+
+/**
+ * Get the minimum snap distance for the given corners
+ */
+export function minCornerSnapDistance(movementDestinationWithoutSnap : Vector, box : Box2, gridSize : Vector) : Vector {
+    const snapFraction = 2;
+    return {
+        x : _getDistance(movementDestinationWithoutSnap.x, box.dimension.x, gridSize.x / snapFraction),
+        y : _getDistance(movementDestinationWithoutSnap.y, box.dimension.y, gridSize.y / snapFraction)
+    }
+}
+
+export function _getDistance(position : number, size : number, gridSize : number) : number {
+    let leftPosition = position - size / 2;
+    let rightPosition = position + size / 2;
+    let leftSnapDistance = roundToMultiple(leftPosition, gridSize) - leftPosition;
+    let rightSnapDistance = roundToMultiple(rightPosition, gridSize) - rightPosition;
+
+    if (Math.abs(leftSnapDistance) < Math.abs(rightSnapDistance)) {
+        return leftSnapDistance;
+    } else {
+        return rightSnapDistance;
+    }
+}
