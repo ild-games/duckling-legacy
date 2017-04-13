a111ce9283f37dc2326b787671b6e24d386142c2
Jeff Swenson <jeffswenson@betterthannull.com>
2017-04-12 20:50:13 -0500

    Fix the path attribute so that it draws again. It was broken by the resize-tool changes.

--- src/duckling/game/path/path-drawer.ts
+++ src/duckling/game/path/path-drawer.ts
@@ -5,7 +5,7 @@ import {Entity} from '../../entitysystem/entity';
 import {drawRectangle} from '../../canvas/drawing/util';
 import {Vector, vectorRotate} from '../../math/vector';
 
-import {getPath} from './path-attribute';
+import {PathAttribute} from './path-attribute';
 
 export const PATH_HEIGHT = 4;
 const VERTEX_MARKER_HEIGHT = 2 * PATH_HEIGHT;
@@ -16,12 +16,11 @@ const PATH_COLOR = 0x607d8b;
 const STARTING_SQUARE_COLOR = 0x00a626;
 const ENDING_SQUARE_COLOR = 0xf44336;
 
-export function drawPathAttribute(entity : Entity) : DrawnConstruct {
-    let pathAttribute = getPath(entity);
+export function drawPathAttribute(pathAttribute : PathAttribute) : DrawnConstruct {
     if (!pathAttribute || pathAttribute.vertices.length === 0) {
         return null;
     }
-    
+
     if (pathAttribute.isLoop) {
         return _drawLoop(pathAttribute.vertices);
     } else {
@@ -41,7 +40,7 @@ function _drawNonLoopPath(vertices : Vector[]) : DisplayObject {
     if (vertices.length === 1) {
         return _drawJoint(vertices[0]);
     }
-    
+
     if (vertices.length === 2) {
         let container = new Container();
         let theta = _pathTheta(vertices[0], vertices[1]);
@@ -50,7 +49,7 @@ function _drawNonLoopPath(vertices : Vector[]) : DisplayObject {
         container.addChild(_drawSquare(vertices[1], theta, ENDING_SQUARE_COLOR));
         return container;
     }
-    
+
     return _drawPath(vertices);
 }
 
@@ -63,7 +62,7 @@ function _drawLoopPath(vertices : Vector[]) : DisplayObject {
         container.addChild(_drawJoint(endVertex));
     }
     container.addChild(_drawLine(vertices[vertices.length - 1], vertices[0]));
-    
+
     // draw the loop indicator last so that it will be drawn on top of other segments
     container.addChild(_drawLoopIndicator(vertices[0], _pathTheta(vertices[0], vertices[1])));
     return container;
@@ -71,7 +70,7 @@ function _drawLoopPath(vertices : Vector[]) : DisplayObject {
 
 function _drawLoopIndicator(vertex : Vector, theta : number) : DisplayObject {
     let container = new Container();
-    
+
     container.addChild(_drawSquare(vertex, 0, STARTING_SQUARE_COLOR));
     container.addChild(_drawTriangle({x: vertex.x + DIRECTION_INDICATOR_OFFSET, y: vertex.y}, STARTING_SQUARE_COLOR));
 
@@ -80,19 +79,19 @@ function _drawLoopIndicator(vertex : Vector, theta : number) : DisplayObject {
 
 function _drawPath(vertices : Vector[]) : DisplayObject {
     let container = new Container();
-    
+
     for (let i = 0; i < vertices.length - 1; i++) {
         let startVertex = vertices[i];
         let endVertex = vertices[i + 1];
         container.addChild(_drawLine(startVertex, endVertex));
         container.addChild(_drawJoint(endVertex));
     }
-    
+
     let firstSquareTheta = _pathTheta(vertices[0], vertices[1]);
     container.addChild(_drawSquare(vertices[0], firstSquareTheta, STARTING_SQUARE_COLOR));
     let secondSquareTheta = _pathTheta(vertices[vertices.length - 2], vertices[vertices.length - 1]);
     container.addChild(_drawSquare(vertices[vertices.length - 1], secondSquareTheta, ENDING_SQUARE_COLOR));
-        
+
     return container;
 }
 
